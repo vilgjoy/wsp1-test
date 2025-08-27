8dc66c8b42cb4d50787cb7d027b05494ddd3b2c8
vilgjoy <vilgot.sandberg@elev.ga.ntig.se>
2025-08-27 10:58:40 +0200

    server-not-work

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ app.get("/", (req, res) => {
     res.send("Hello world!")
 })
 
+app.get("/about", (req, res) => {
+    res.json({
+        "message": "Hatisk textbox"
+    })
+})
+
 app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000")
 })
