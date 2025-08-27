91f309f949891f119fe98d01c77d493fcc1e805d
vilgjoy <vilgot.sandberg@elev.ga.ntig.se>
2025-08-27 10:40:30 +0200

    start

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.get("/", (req, res) => {
 })
 
 app.listen(3000, () => {
-    console.log("Server is running on https://localhost:3000")
+    console.log("Server is running on http://localhost:3000")
 })
