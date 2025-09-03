ebb6eaee3b3a083d55faa6c14b04fa7f3c4b6f35
vilgjoy <vilgot.sandberg@elev.ga.ntig.se>
2025-09-03 11:01:46 +0200

    nav njk fil

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 import express from "express"
+import nunjucks from "nunjucks"
 
 const app = express()
 
 
+app.use(express.static("public"))
+
+nunjucks.configure("views", {
+    autoescape: true,
+    express: app
+})
+
 app.get("/", (req, res, next) => {
-    console.log(req)
     res.send("Hello world!")
 })
 
+// Skriv en ny route, till about sidan
+// skapa about sidan, som en njk templat
+// på about sidan, ska det finnas en länk till er github,
+// en bild och en text om att detta är ett skolarbete
+
 app.get("/about", (req, res) => {
-    res.json({
-        "message": "Hatisk textbox"
+    res.render("index.njk", {
+        title: "vår första dyna sida",
+        message: "nunjucks, more like"
     })
 })
 
