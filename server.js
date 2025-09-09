c23d51e9892ebe3e442490448d3dadf2d5598560
vilgjoy <vilgot.sandberg@elev.ga.ntig.se>
2025-09-09 09:43:10 +0200

    query parameters

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import express from "express"
 import nunjucks from "nunjucks"
+import morgan from "morgan"
 
 const app = express()
 
-
+app.use(morgan("dev"))
 app.use(express.static("public"))
 
 nunjucks.configure("views", {
@@ -20,11 +21,21 @@ app.get("/", (req, res, next) => {
 // på about sidan, ska det finnas en länk till er github,
 // en bild och en text om att detta är ett skolarbete
 
-app.get("/about", (req, res) => {
+app.get("/index", (req, res) => {
     res.render("index.njk", {
         title: "vår första dyna sida",
         message: "nunjucks, more like"
     })
+})
+
+// En query parameter är en del av en webbadress (URL) som används för att skicka extra information till servern.
+// Exempel nedan är en route some tar emot två query parameters, name och message.
+// så för att använda message och name så efter localhost:3000/ skriver vi:
+// /greeting (för att komma till rätt route) ?name=(vad som helst)&message=(vad som helst)
+
+app.get("/greeting", (req, res) => {
+    console.log(req.query)
+    res.send(`tjena ${req.query.name}, ${req.query.message}`)
 })
 
 app.listen(3000, () => {
